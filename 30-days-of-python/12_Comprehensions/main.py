4cf0d58e1649c11cf9ef02403df4e6fb362ad041
Saadwarraich7223 <saadwarraich7223@gmail.com>
2026-06-27 10:33:42 +0500

    Day 12: Comprehensions -done

--- 30-days-of-python/12_Comprehensions/main.py
+++ 30-days-of-python/12_Comprehensions/main.py
@@ -1,5 +1,14 @@
-# Day 12 — Comprehensions
+# Day 12 ï¿½ Comprehensions
 # Date: July 05, 2026
 #
 # Learning Goals:
 # TODO: Fill in what you learned today, code exercises, and notes.
+
+numbers=[1,2,3,4,5,6,7,8,9,10]
+even =[num for num in numbers if num%2 ==0]
+print(even)
+sqr=[num**2 for num in numbers]
+print(sqr)
+
+# for num in num:
+#     print(num**2)
